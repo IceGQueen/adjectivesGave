06fa186de3c79eae8c27f24fc3453028a38b827f
IceGQueen <sorayabless@gmail.com>
2016-05-17 23:16:26 +0300

    "playing with the buttons"

--- game.js
+++ game.js
@@ -1,7 +1,25 @@
 "use strict";
 
-var btnHello = new Button("hello");
-btnHello.add();
+var mainMenu = new Page("Main menu");
 
-var btnWorld = new Button("world");
-btnWorld.add();
+var btnstart = new Button("Start", function() {
+    gamePage.show();
+});
+mainMenu.addElement(btnstart);
+
+var btnsettings = new Button("Settings",function (){settingsPage.show();});
+mainMenu.addElement(btnsettings);
+
+mainMenu.show();
+
+var gamePage = new Page("Game");
+var btnBack = new Button("Back to main menu", function() {
+    mainMenu.show();
+});
+gamePage.addElement(btnBack);
+
+var settingsPage = new Page ("Settings");
+var btnBack1 = new Button("Back to main menu", function() {
+    mainMenu.show();
+});
+settingsPage.addElement(btnBack1);
--- library.js
+++ library.js
@@ -2,19 +2,65 @@
  * Created by betrayer on 05.05.16.
  */
 "use strict";
-var Button = function Button(text) {
+var Button = function Button(text, callback) {
     this._text = text;
+    this._callback = callback;
     this._element = document.createElement("input");
     this._element.setAttribute("type", "button");
     this._element.setAttribute("value", this._text);
+    this._element.setAttribute("class", "button");
 
-    this._element.style.backgroundColor = "pink";
-    this._element.style.display = "block";
-    this._element.style.fontSize = "20px";
-    this._element.style.border = "1px solid gray";
-    this._element.style.borderRadius = "4px";
+    this._element.addEventListener('click', Button.prototype._onClick.bind(this), false)
 };
 
 Button.prototype.add = function() {
     document.body.appendChild(this._element);
+};
+
+Button.prototype._onClick = function (){
+    if (this._callback) {
+        this._callback();
+    } else {
+        console.log ("You forgot to give callback in the constructor of button named " + this._text);
+    }
+};
+
+
+
+
+var Page = function Page(name) {
+    this._name = name;
+
+    this._header = document.createElement("div");
+    this._header.setAttribute("class", "page-header");
+    this._header.innerHTML = this._name;
+    this._content = document.createElement("div");
+    this._content.setAttribute("class", "page-content");
+
+    this._content.appendChild(this._header);
+
+    this._elements = [];
+};
+
+Page.prototype.addElement = function(element) {
+    this._elements.push(element);
+    this._content.appendChild(element._element);
+};
+
+Page.prototype.show = function() {
+    if (window.currentPage) {
+        if (window.currentPage === this) {
+            return;
+        }
+        window.currentPage.hide();
+    }
+    document.body.appendChild(this._content);
+    window.currentPage = this;
+};
+
+Page.prototype.hide = function() {
+    if (window.currentPage === this) {
+        document.body.removeChild(this._content);
+        delete window.currentPage;
+    }
 };
